045d2d502222a7e600615db1d6d1edf94f2f9b8d
dunsinadesina <adesinajesudunsin@gmail.com>
2024-07-11 06:59:16 -0700

    creates unique table for users

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- Create table with users
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+	id INT NOT NULL AUTO_INCRMEMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	country CHAR(2) NOT NULL DEFAULT 'US' CHECK (country IN ('US', 'CO', 'TN'))
+);
